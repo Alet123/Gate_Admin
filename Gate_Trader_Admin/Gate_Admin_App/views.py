90992cd056104bb87480c2cf349b3d732896c0dc
gfluser <82381562+Alet123@users.noreply.github.com>
2025-01-20 18:52:35 +0530

    gtadmin

---
+++ Gate_Trader_Admin/Gate_Admin_App/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def login(request):
+    return render(request, "login.html")
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def trade_graph(request):
+    return render(request, "trade_graph.html")
+
+
+def portfolio(request):
+    return render(request, "portfolio.html")
+
+
+def trading_accounts(request):
+    return render(request, "trading_accounts.html")
+
+
+def create_trading_accounts(request):
+    return render(request, "create_trading_accounts.html")
+
+
+def group_accounts(request):
+    return render(request, "group_accounts.html")
+
+
+def create_group_accounts(request):
+    return render(request, "create_group_accounts.html")
+
+
+def master_accounts(request):
+    return render(request, "master_accounts.html")
+
+
+def create_master_accounts(request):
+    return render(request, "create_master_accounts.html")
+
+
+def api(request):
+    return render(request, "api.html")
